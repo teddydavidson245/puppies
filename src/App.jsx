18716389a3c31621b2fefc0bcadaf48558017910
Teddymutiga <teddydavidson245@gmail.com>
2023-07-16 21:17:24 -0500

    puppies + id

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import {puppyList} from './data.js'
+import './index.css'
 
 function App() {
-  const [puppies, setPuppies] = useState(puppyList)
-  console.log(puppyList)
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log(puppyList);
+  const [featPupId, setFeatPupId] = useState(null);
+  const featuredPup = puppies.find((pup)=> pup.id === featPupId) 
   return (
     <>
       <div>
@@ -18,9 +21,24 @@ function App() {
         </a>
         {
           puppies.map((puppy) => {
-            return <p>{puppy.name}</p>
+            return <p onClick={()=>{setFeatPupId(puppy.id)}} key={puppy.id}>{puppy.name}</p>
           })
-        }
+        
+      }
+        
+        {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )};
+  
+        
+        
+        
       </div>
       
     </>
